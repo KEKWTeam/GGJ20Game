ed382eb623270d9f465cbfaf549ed88d518a8ee2
SrRusty <kv.mor.ga@gmail.com>
2020-02-01 18:51:17 +0100

    KevinMG
    dsadas

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ShowScore()
+    {
+        SceneManager.LoadScene("Score");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit up");
+        Application.Quit();
+    }
+}
